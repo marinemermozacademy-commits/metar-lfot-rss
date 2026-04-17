6624c148c47129c2732d48c94dad086db45db985
marinemermozacademy-commits <marine.mermozacademy@gmail.com>
2026-04-17 15:03:12 +0200

    Update generate_rss.py

--- generate_rss.py
+++ generate_rss.py
@@ -1,37 +1,40 @@
 import requests
 import os
-from datetime import datetime
 
 API_KEY = os.environ["CHECKWX_API_KEY"]
 ICAO = "LFOT"
 
 headers = {"X-API-Key": API_KEY}
 
-# METAR
-metar_url = f"https://api.checkwx.com/metar/{ICAO}"
+# METAR (version stable)
+metar_url = f"https://api.checkwx.com/v2/metar/{ICAO}/raw"
 metar_data = requests.get(metar_url, headers=headers).json()
-metar = metar_data["data"][0]
 
-# TAF
+metar = "METAR indisponible"
+if "data" in metar_data and len(metar_data["data"]) > 0:
+    metar = metar_data["data"][0]
+
+# TAF (version stable)
 taf_url = f"https://api.checkwx.com/v2/taf/{ICAO}/raw"
 taf_data = requests.get(taf_url, headers=headers).json()
-taf = taf_data["data"][0]
 
-now = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
+taf = "TAF indisponible"
+if "data" in taf_data and len(taf_data["data"]) > 0:
+    taf = taf_data["data"][0]
 
+# FORMAT AFFICHAGE SIGNAO
 description = f"""LFOT<br><br>
-🟢 METAR {metar.replace("METAR ", "")}<br><br>
-🟡 TAF {taf.replace("TAF ", "")}
+🟢 {metar.replace("METAR ", "")}<br><br>
+🟡 {taf.replace("TAF ", "")}
 """
 
 rss = f"""<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
 <channel>
 <title>METAR LFOT</title>
-<description>Briefing météo LFOT</description>
 
 <item>
-<title>METAR LFOT</title>
+<title>LFOT METAR/TAF</title>
 <description>{description}</description>
 </item>
 
